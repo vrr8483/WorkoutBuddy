500972a5676c6ef9f80c1c228470812f37cf0479
Peter <4peetah@gmail.com>
2018-01-27 16:16:22 -0500

    Began work on Log in system

--- WorkoutBuddy/Account/views.py
+++ WorkoutBuddy/Account/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate
 
 def login(request):
-    return HttpResponse("Logging in.")
+    if request.GET.get('username') is None or request.GET.get("password") is None:
+        return HttpResponse(render(request, 'energize_andover/Login.html', {'message': "Login Failed: Missing Username and/or Password"}))
+    user = authenticate(username=request.GET.get('username'), password=request.GET.get('password'))
+    if user is not None:
+        request.session['logged_in'] = True;
+        request.session['username'] = request.GET.get('username')
+        request.session['password'] = request.GET.get('password')
+        message = "User " + request.session['username'] + " logged in."
+        return HttpResponseRedirect(request.session['destination'])
+    else:
+        return HttpResponse(render(request, 'energize_andover/Login.html', {'message': "Login Failed: Missing Username and/or Password"}))
